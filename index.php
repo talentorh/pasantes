cf687c9be58dc5992f53fb7aca5dc3d5ae13f374
dany22ag <danytesiisc@gmail.com>
2024-07-04 07:22:58 -0600

    actualizacion

--- index.php
+++ index.php
@@ -16,6 +16,43 @@
             text-transform: uppercase;
         }
     </style>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<div class="container-xxl" style="width: 100%; height:100%; background-color: #85C7E3; padding:5px;">
+
+<div style="width: 100%; height: auto; display: flex; align-items: center; justify-content: center;">
+
+<img src="img\OIP.jpg" class="img-fluid" style="width:30px; height:30px; border-radius: 5px;">&nbsp;
+</div>
+<form id="guardardatos" name="guardardatos">
+<div id="mensaje"></div>
+<script>
+                $("#guardardatos").on("submit", function(e) {
+                    e.preventDefault();
+                    var formData = new FormData(document.getElementById("guardardatos"));
+                    formData.append("dato", "valor");
+                    $.ajax({
+                        url: "guardarInduccion.php",
+                        type: "post",
+                        data: formData,
+                        contentType: false,
+                        processData: false,
+                        beforeSend: function(objeto) {
+                            $('#mensaje').html('<div id="mensaje" style="position: fixed;  top: 0px; left: 0px;  width: 100%; height: 100%; z-index: 9999;  opacity: .7; background: url(../imagenes/loader.gif) 50% 50% no-repeat rgb(249,249,249);"><br/></div>');
+                        },
+                        success: function(data) {
+                            $("#mensaje").html(data);
+                        }
+                    })
+                })
+            </script>
+
+<div style="width: 100%; height: 100%; background-color: white; border-radius: 10px; padding: 10px; font-size: 15px; margin-top: 10px;">
+<div class="container" id="contenedor">
+<div style="width: 100%; height: 100%;">
+
     <div class="container">
 <div>
         <h2 id="h2">Guía de inducción al área y puesto de personal en el puesto</h2>
@@ -52,12 +89,12 @@
     <h3>Tipo de movimiento:</h3>
 </div>
 <div class="form-check">
-    <input class="form-check-input" type="radio" value="" id="flexCheckDefault">
-    <label class="form-check-label" for="flexCheckDefault">NUEVO INGRESO</label>
+    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+    <label class="form-check-label" for="flexRadioDefault1">INGRESO<label>
 </div>
 <div class="form-check">
-    <input class="form-check-input" type="radio" value="" id="flexCheckDefault">
-    <label class="form-check-label" for="flexCheckDefault">REINGRESO</label>
+    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+    <label class="form-check-label" for="flexRadioDefault1">REINGRESO</label>
 </div>
 <div>
     <strong>Inducción a la Unidad de adscripción:</strong>
@@ -67,67 +104,68 @@
     <label class="form-check-label" for="flexCheckDefault">1. Dar la bienvenida a la trabajador/a de nuevo ingreso a Servicios de Salud IMSS-BIENESTAR</label>
 </div>
 <div class="form-check">
-    <input>
-    <label>2. Explique la organización de la Unidad con el apoyo del organigrama</label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">2. Explique la organización de la Unidad con el apoyo del organigrama</label>
 </div>
 <div class="form-check">
-    <input>
-    <label>3. Realice un recorrido por las instalaciones de la Unidad</label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">3. Realice un recorrido por las instalaciones de la Unidad</label>
 </div>
 <div class="form-check">
-    <input>
-    <label>4. Presente al trabajador/a con los y las responsables de otras áreas, en caso de tener trato con ellos</label>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">4. Presente al trabajador/a con los y las responsables de otras áreas, en caso de tener trato con ellos</label>
 </div>
 <div>
     <strong>Inducción al área de trabajo</strong>
 </div>
-<div>
-    <input>
-    <label>1. Describa brevemente el área de trabajo y los procesos en los que están involucrados</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">1. Describa brevemente el área de trabajo y los procesos en los que están involucrados</label>
 </div>
-<div>
-    <input>
-    <label>2. Presente a las compañeras y compañeros del grupo de trabajo</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">2. Presente a las compañeras y compañeros del grupo de trabajo</label>
 </div>
-<div>
-    <input>
-    <label>3. Propicie un ambiente de cordialidad e integración de equipo</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">3. Propicie un ambiente de cordialidad e integración de equipo</label>
 </div>
-<div>
-    <input>
-    <label>4. Comente condiciones generales del ttrabajo de IMSS-BIENESTAR</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">4. Comente condiciones generales del ttrabajo de IMSS-BIENESTAR</label>
 </div>
 <div>
     <strong>Inducción al puesto</strong>
 </div>
-<div>
-    <input>
-    <label>1. Explique la misión del puesto</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">1. Explique la misión del puesto</label>
 </div>
-<div>
-    <input>
-    <label>2. Señale la visión que se desea del puesto</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">2. Señale la visión que se desea del puesto</label>
 </div>
-<div>
-    <input>
-    <label>3. Muestra la ubicación del puesto de trabajo y su correspondencia estructural</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">3. Muestra la ubicación del puesto de trabajo y su correspondencia estructural</label>
 </div>
-<div>
-    <input>
-    <label>4. Explique los formatos y el uso de estos</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">4. Explique los formatos y el uso de estos</label>
 </div>
-<div>
-    <input>
-    <label>5. Capacite en la utilización de equipos y materiales de trabajo</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">5. Capacite en la utilización de equipos y materiales de trabajo</label>
 </div>
-<div>
-    <input>
-    <label>6. Explique las instrucciones básicas de seguridad e higiene</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">6. Explique las instrucciones básicas de seguridad e higiene</label>
 </div>
-<div>
-    <input>
-    <label>7. Motive al trabajador/a para que formule preguntas y aclare dudas sobre sus actividades y las necesidades o expectativas a cubrir con relación a su puesto</label>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">7. Motive al trabajador/a para que formule preguntas y aclare dudas sobre sus actividades y las necesidades o expectativas a cubrir con relación a su puesto<br><br></label>
 </div>
-</div> <!--cierra segundo contenedor-->
+</div>
+    </div>
 </body>
 </html>
