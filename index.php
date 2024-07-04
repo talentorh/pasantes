5d13a2b3cca09ac83995c9c40b4a83066d4a2547
dany22ag <danytesiisc@gmail.com>
2024-07-03 18:25:21 -0600

    creacion de formulario

--- index.php
+++ index.php
@@ -1,13 +1,123 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CHECKLIST DE GUIA DE INDUCCION</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Hola<h1>
+    <div class="container">
+<div>
+        <h2>Guía de inducción al área y puesto de personal en el puesto</h2>
+        <h2>MEDICO ESPECIALISTA HOSPITAL</h2>
+    </div>
+    <div>
+        <label>Nombre del trabajador:</label>
+        <input type="text">
+</div>
+<div>
+    <label>CURP:</label>
+    <input type="text">
+</div>
+<div>
+    <label>Puesto:</label>
+    <input type="text">
+</div>
+<div>
+    <label>Adscripcion:</label>
+    <input type="text">
+</div>
+<div>
+    <label>RFC:</label>
+    <input type="text">
+</div>
+<div>
+    <label>CLUES:</label>
+    <input type="text">
+</div>
+</div>
+<div class="container2">
+<div>
+    <h3>Tipo de movimiento:</h3>
+</div>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">NUEVO INGRESO</label>
+</div>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">REINGRESO</label>
+</div>
+<div>
+    <strong>Inducción a la Unidad de adscripción:</strong>
+</div>
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">1. Dar la bienvenida a la trabajador/a de nuevo ingreso a Servicios de Salud IMSS-BIENESTAR</label>
+</div>
+<div class="form-check">
+    <input>
+    <label>2. Explique la organización de la Unidad con el apoyo del organigrama</label>
+</div>
+<div class="form-check">
+    <input>
+    <label>3. Realice un recorrido por las instalaciones de la Unidad</label>
+</div>
+<div class="form-check">
+    <input>
+    <label>4. Presente al trabajador/a con los y las responsables de otras áreas, en caso de tener trato con ellos</label>
+</div>
+<div>
+    <strong>Inducción al área de trabajo</strong>
+</div>
+<div>
+    <input>
+    <label>1. Describa brevemente el área de trabajo y los procesos en los que están involucrados</label>
+</div>
+<div>
+    <input>
+    <label>2. Presente a las compañeras y compañeros del grupo de trabajo</label>
+</div>
+<div>
+    <input>
+    <label>3. Propicie un ambiente de cordialidad e integración de equipo</label>
+</div>
+<div>
+    <input>
+    <label>4. Comente condiciones generales del ttrabajo de IMSS-BIENESTAR</label>
+</div>
+<div>
+    <strong>Inducción al puesto</strong>
+</div>
+<div>
+    <input>
+    <label>1. Explique la misión del puesto</label>
+</div>
+<div>
+    <input>
+    <label>2. Señale la visión que se desea del puesto</label>
+</div>
+<div>
+    <input>
+    <label>3. Muestra la ubicación del puesto de trabajo y su correspondencia estructural</label>
+</div>
+<div>
+    <input>
+    <label>4. Explique los formatos y el uso de estos</label>
+</div>
+<div>
+    <input>
+    <label>5. Capacite en la utilización de equipos y materiales de trabajo</label>
+</div>
+<div>
+    <input>
+    <label>6. Explique las instrucciones básicas de seguridad e higiene</label>
+</div>
+<div>
+    <input>
+    <label>7. Motive al trabajador/a para que formule preguntas y aclare dudas sobre sus actividades y las necesidades o expectativas a cubrir con relación a su puesto</label>
+</div>
+</div> <!--cierra segundo contenedor-->
 </body>
 </html>
