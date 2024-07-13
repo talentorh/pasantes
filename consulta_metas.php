f47a641ca3fa49da3bc7806acdce1cb85004ad02
dany22ag <danytesiisc@gmail.com>
2024-07-13 09:51:02 -0600

    Actualizacion donde las celdas y multiceldas ya quedan alineadas aunque el contenido que tengan sea largo

---
+++ consulta_metas.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(0);
+require 'clases/conexion.php';
+
+try {
+    $conexion = new Conexion();
+    $id = 1108;
+
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
+    $conexion->beginTransaction();
+
+    $sql = $conexion->prepare("SELECT * FROM descripcionesmetas2023 WHERE id_empleado = :id_empleado");
+    $sql->execute([':id_empleado' => $id]);
+    $datos = $sql->fetch();
+
+    if ($datos) {
+        $conexion->commit();
+        $idempleado = $datos['id_empleado'];
+
+        if ($id == $idempleado) {
+            require 'pdf_cedula_metas.php';
+        }
+    } else {
+        $conexion->rollBack();
+        echo "No se encontraron datos para el empleado con ID: $id";
+    }
+} catch (PDOException $e) {
+    $conexion->rollBack();
+    echo "Error: " . $e->getMessage();
+}
+?>
+
+
+
