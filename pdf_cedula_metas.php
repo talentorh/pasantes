4f88ac5f15066d2a6f13cd4a9d811bc09e5e2a1e
Alberto Vazquez <beto_1866@outlook.com>
2024-07-18 09:11:37 -0600

    carga de cambios

--- pdf_cedula_metas.php
+++ pdf_cedula_metas.php
@@ -20,35 +20,37 @@ $pdf = new PDF("L", "mm", "LETTER");
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',8);
-$pdf->cell(258,5,'DATOS DEL EVALUADO',0,1,'L',0);
-$pdf->cell(30,5,'No. NOMINA',0,0,'L',0);
+$pdf->cell(258,5,'DATOS DEL EVALUADO',1,1,'C',0);
+$pdf->Ln(1);
+$pdf->cell(30,5,'No. de Empleado',0,0,'L',0);
 $pdf->SetFont('Arial','',8);
-$pdf->cell(100,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
+$pdf->cell(228,5,mb_convert_encoding('1983','ISO-8859-1','UTF-8'),1,1,'C',0);
 $pdf->SetFont('Arial','B',8);
-$pdf->cell(30,5,'NOMBRE',0,0,'L',0);
+$pdf->cell(30,5,'Nombre',0,0,'L',0);
 $pdf->SetFont('Arial','',8);
-$pdf->cell(100,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
+$pdf->cell(228,5,mb_convert_encoding('ALBERTO VAZQUEZ ARIAS','ISO-8859-1','UTF-8'),1,1,'C',0);
 $pdf->SetFont('Arial','B',8);
-$pdf->cell(30,5,'PUESTO',0,0,'L',0);
+$pdf->cell(30,5,'Puesto',0,0,'L',0);
 $pdf->SetFont('Arial','',8);
-$pdf->cell(100,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
+$pdf->cell(228,5,mb_convert_encoding('SOPORTE ADMINSTRATIVO B','ISO-8859-1','UTF-8'),1,1,'C',0);
 $pdf->SetFont('Arial','B',8);
 $pdf->ln(5);
-$pdf->cell(258,5,'DATOS DEL EVALUADOR',0,1,'L',0);
-$pdf->cell(30,5,'No. NOMINA',0,0,'L',0);
+$pdf->cell(258,5,'DATOS DEL EVALUADOR',1,1,'C',0);
+$pdf->Ln(1);
+$pdf->cell(30,5,'No. de Empleado',0,0,'L',0);
 $pdf->SetFont('Arial','',8);
-$pdf->cell(100,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
+$pdf->cell(228,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
 $pdf->SetFont('Arial','B',8);
-$pdf->cell(30,5,'NOMBRE',0,0,'L',0);
+$pdf->cell(30,5,'Nombre',0,0,'L',0);
 $pdf->SetFont('Arial','',8);
-$pdf->cell(100,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
+$pdf->cell(228,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
 $pdf->SetFont('Arial','B',8);
-$pdf->cell(30,5,'PUESTO',0,0,'L',0);
+$pdf->cell(30,5,'Puesto',0,0,'L',0);
 $pdf->SetFont('Arial','',8);
-$pdf->cell(100,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
+$pdf->cell(228,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,1,'C',0);
 $pdf->ln(5);
 $pdf->Setfillcolor(3,19,100);
-$pdf->cell(258,7,mb_convert_encoding('','ISO-8859-1','UTF-8'),0,1,'C',1);
+$pdf->cell(258,7,mb_convert_encoding('METAS ESTRATEGICAS','ISO-8859-1','UTF-8'),0,1,'C',1);
 //contador para que ponga el numero de la meta depende de cuantas son
 
 //consulta y validacion para que si existen metas las coloque
@@ -56,75 +58,68 @@ $pdf->cell(258,7,mb_convert_encoding('','ISO-8859-1','UTF-8'),0,1,'C',1);
 //codigo de metas
 $pdf->ln(5);
 $pdf->SetFont('Arial','',8);
-$pdf->cell(10,5,'',0,0,'C',0);//número de meta
-$pdf->cell(3);
+//número de meta
+
 $pdf->cell(20,5,mb_convert_encoding('Eje estratégico','ISO-8859-1','UTF-8'),0,0,'L',0);
 $pdf->cell(194);
-$pdf->cell(20,5,mb_convert_encoding('Fecha de cumplimiento de meta','ISO-8859-1','UTF-8'),0,1,'L',0,);
-$pdf->cell(13);
-$pdf->multicell(210,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'C',0);
+$pdf->cell(20,5,mb_convert_encoding('Fecha de cumplimiento','ISO-8859-1','UTF-8'),0,1,'L',0,);
+
+$pdf->multicell(210,5,mb_convert_encoding('Modelo de Desarrollo Organizacional integral.','ISO-8859-1','UTF-8'),1,'C',0);
 $pdf->ln(-5);
-$pdf->cell(228);
-$pdf->cell(30,5,'',1,1,'C',0); //contenido ponderación
-$pdf->cell(13);
+$pdf->cell(215);
+$pdf->cell(30,5,'',1,1,'C',0);
+
 $pdf->cell(40,5,mb_convert_encoding('Línea Estratégica alineada a','ISO-8859-1','UTF-8'),0,0,'L',0);
 $pdf->cell(175);
 $pdf->cell(20,5,'Unidad de medida',0,1,'L',0);
-$pdf->cell(13);
-$pdf->multicell(210,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'C',0);
+
+$pdf->multicell(210,5,mb_convert_encoding('Impulsar el cambio tecnológico e innovación','ISO-8859-1','UTF-8'),1,'C',0);
+
 $pdf->ln(-5);
-$pdf->cell(228);
-$pdf->cell(30,5,'',1,1,'C',0); //contenido avance
-$pdf->cell(13);
+$pdf->cell(215);
+$pdf->cell(30,5,'',1,1,'C',0);
+
 $pdf->cell(10,5,'Meta',0,0,'L',0);
 $pdf->cell(205);
 $pdf->cell(20,5,mb_convert_encoding('Ponderación','ISO-8859-1','UTF-8'),0,1,'L',0);//titulo tercer campo
-$pdf->cell(13);
-$pdf->multicell(210,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'C',0);
+
+$pdf->multicell(210,5,mb_convert_encoding('Investigar una herramientas de inteligencia artificial que pueda ser aplicada en el sistema de evaluación del desempeño, esto con el objetivo de que el personal pueda redactar sus metas de una manera mas eficiente, solicitando a la IA(Inteligencia Artificial) Asesoría sobre algún tema en especifico.','ISO-8859-1','UTF-8'),1,'C',0);
+
 $pdf->ln(-5);
-$pdf->cell(228);
-$pdf->cell(30,5,'',1,1,'C',0); //contenido avance
-$pdf->cell(13);
+$pdf->cell(215);
+$pdf->cell(30,5,'',1,1,'C',0);
+
 $pdf->cell(40,5,'Indicador',0,1,'L',0);
-$pdf->cell(13);
+
 $pdf->multicell(210,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'C',0);
-$pdf->cell(13);
-$pdf->ln(3);
-$pdf->cell(13);
-$pdf->cell(30,5,'campo 1',0,0,'L',0);
+$pdf->Ln(5);
+$pdf->cell(258,5,'Niveles de cumplimiento',1,1,'C',0);
+$pdf->Ln(2);
+$pdf->cell(30,5,'Deficiente',0,0,'L',0);
 $pdf->cell(18);
-$pdf->cell(30,5,'campo 2',0,0,'L',0);
+$pdf->cell(30,5,'No aprobatorio',0,0,'L',0);
 $pdf->cell(20);
-$pdf->cell(30,5,'campo 3',0,0,'L',0);
+$pdf->cell(30,5,'No satisfactorio',0,0,'L',0);
 $pdf->cell(20);
-$pdf->cell(30,5,'campo 4',0,0,'L',0);
+$pdf->cell(30,5,'Satisfactorio',0,0,'L',0);
 $pdf->cell(21);
-$pdf->cell(30,5,'campo 5',0,1,'L',0);
-$pdf->cell(13);
+$pdf->cell(30,5,'Sobresaliente',0,1,'L',0);
+
+$pdf->multicell(45,5,mb_convert_encoding('Investigar una herramientas de inteligencia artificial que pueda ser aplicada en el sistema de','ISO-8859-1','UTF-8'),1,'L',0);
+$pdf->ln(-5);
+$pdf->cell(48);
+$pdf->multicell(45,5,mb_convert_encoding('Investigar una herramientas de inteligencia artificial que pueda ser aplicada en el sistema de','ISO-8859-1','UTF-8'),1,'L',0);
+$pdf->ln(-5);
+$pdf->cell(99);
 $pdf->multicell(45,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'L',0);
 $pdf->ln(-5);
-$pdf->cell(62);
+$pdf->cell(149);
 $pdf->multicell(45,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'L',0);
 $pdf->ln(-5);
-$pdf->cell(112);
-$pdf->multicell(45,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'L',0);
-$pdf->ln(-5);
-$pdf->cell(162);
-$pdf->multicell(45,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'L',0);
-$pdf->ln(-5);
-$pdf->cell(213);
+$pdf->cell(200);
 $pdf->multicell(45,5,mb_convert_encoding('','ISO-8859-1','UTF-8'),1,'L',0);
 //después de haber validado y colocado el número de metas se coloca el valor de grado de cumplimiento
-$pdf->SetFont('Arial','B',8);
-$pdf->ln(3);
-$pdf->cell(2);
-$pdf->cell(256,5,'--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------',0,1,'C',0);
-$pdf->ln(3);
-$pdf->cell(10);
-$pdf->cell(83,5,'VALOR DE GRADO DE CUMPLIMIENTO DE LOS OBJETIVOS',0,0,'C',0);
-$pdf->SetFont('Arial','',8);
-$pdf->cell(50);
-$pdf->cell(15,5,'',1,1,'C',0);
+
 //termina contenido de metas que será dinámico
 
 //inicia meta funcional y estas son fijas y es 1
@@ -134,7 +129,7 @@ $pdf->cell(258,7,mb_convert_encoding('','ISO-8859-1','UTF-8'),0,1,'C',1);
 $pdf->ln(5);
 $pdf->SetFont('Arial','',8);
 $pdf->cell(10,5,'',0,0,'C',0);//número de meta
-$pdf->cell(3);
+
 $pdf->cell(20,5,mb_convert_encoding('Eje estratégico','ISO-8859-1','UTF-8'),0,0,'L',0);
 $pdf->cell(194);
 $pdf->cell(20,5,mb_convert_encoding('Ponderación','ISO-8859-1','UTF-8'),0,1,'L',0,);
